0b75abf3276dd62d31c08782641ecea1e2de589a
Wilfredo Sulbaran <wilfredosulbaran94@gmail.com>
2022-04-13 18:43:37 -0500

    update webpack

--- next.config.js
+++ next.config.js
@@ -3,7 +3,12 @@
 const dev = process.env.NODE_ENV !== 'production';
 module.exports = {
     env: {
-        BASE_URL: dev ? 'http://localhost:4000/' : 'https://test-tecnical-red-valley.herokuapp.com/',
+        BASE_URL: dev ? 'http://localhost:5000/' : 'https://test-tecnical-red-valley.herokuapp.com/',
         API_KEY_GIPHY: "21W55uc1dqNobqkOtluZMLGnvA2hXAyN"
+    },
+    webpack: config => {
+        config.optimization.minimize = false;        
+        return config;
     }
+
 }
