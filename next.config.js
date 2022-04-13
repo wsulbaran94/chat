4f6cfa7b3dd30995303cf778806048781cbcfdbf
Wilfredo Sulbaran <wilfredosulbaran94@gmail.com>
2022-04-13 17:57:04 -0500

    update

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 const dev = process.env.NODE_ENV !== 'production';
 module.exports = {
     env: {
-        BASE_URL: dev ? 'http://localhost:4000/' : 'https://tecnical-test-rv.herokuapp.com/',
+        BASE_URL: dev ? 'http://localhost:4000/' : 'https://test-tecnical-red-valley.herokuapp.com/',
         API_KEY_GIPHY: "21W55uc1dqNobqkOtluZMLGnvA2hXAyN"
     }
 }
